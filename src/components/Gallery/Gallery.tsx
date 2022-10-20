b8ee27ea7a6de4485d4549599abce7d3dcf73b69
Sahand <sahand.masoleh@gmail.com>
2022-10-21 00:54:24 +0200

    style: mobile carousel

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,5 +1,7 @@
 import "./Gallery.scss";
 import React, { useState } from "react";
+import { ReactComponent as NextIcon } from "@assets/icon-next.svg";
+import { ReactComponent as PreviousIcon } from "@assets/icon-previous.svg";
 import { products } from "@root/productList";
 const PRODUCT = products[0];
 
@@ -8,6 +10,17 @@ function Gallery() {
 
 	function handleActive(id: number) {
 		setActive(id);
+	}
+
+	function handleNavigate(direction: 1 | -1) {
+		setActive((prevActive) => {
+			const neuActive = prevActive + direction;
+			if (neuActive < PRODUCT.images.length && neuActive >= 0) {
+				return neuActive;
+			} else {
+				return prevActive;
+			}
+		});
 	}
 
 	const imagesMap = [];
@@ -34,7 +47,23 @@ function Gallery() {
 	}
 	return (
 		<section className="gallery">
-			<div className="gallery__carousel image">{imagesMap}</div>
+			<div className="gallery__carousel image">
+				{imagesMap}
+				<button
+					className="gallery__control image"
+					onClick={() => handleNavigate(-1)}
+					hidden={active <= 0}
+				>
+					<PreviousIcon className="image__img" />
+				</button>
+				<button
+					className="gallery__control"
+					onClick={() => handleNavigate(1)}
+					hidden={active >= PRODUCT.images.length - 1}
+				>
+					<NextIcon className="image__img" />
+				</button>
+			</div>
 			<div className="gallery__thumbnail-container">{thumbnailsMap}</div>
 		</section>
 	);
